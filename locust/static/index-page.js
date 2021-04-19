371e0dc55b85f0525610532f03d73c47a8eecf53
Emily Edmondson <emily@bevy.com>
2021-04-19 16:06:08 -0500

    wip

--- locust/static/index-page.js
+++ locust/static/index-page.js
@@ -1,45 +1,45 @@
 function removeNoneTextFromTestsSelected() {
-    var tests_selected_artificial_list = document.getElementById('tests_selected_text_area');
-    if(tests_selected_artificial_list.textContent == 'None Selected') {
-        tests_selected_artificial_list.setAttribute('textContent', '');
+    var tests_selected_artificial_list = document.getElementById("tests_selected_text_area");
+    if(tests_selected_artificial_list.textContent == "None Selected") {
+        tests_selected_artificial_list.setAttribute("textContent", "");
     }
 }
 
 function addNoneTextToTestsSelected() {
-    var tests_selected_artificial_list = document.getElementById('tests_selected_text_area');
+    var tests_selected_artificial_list = document.getElementById("tests_selected_text_area");
     if(tests_selected_artificial_list.children.length == 0) {
-        tests_selected_artificial_list.setAttribute('', 'None Selected');
+        tests_selected_artificial_list.setAttribute("", "None Selected");
     }
 
 }
 
 function createRemoveTestButton(test_value) {
-    var button = document.createElement('i');
+    var button = document.createElement("i");
     button.setAttribute("class", "far fa-times-circle");
-    button.setAttribute('id', test_value.concat("_removal"))
+    button.setAttribute("id", test_value.concat("_removal"));
     return button;
 }
 
 function createDivForSelectedTest(test) {
-    var test = document.createElement('div');
-    test.setAttribute('class', "test_selection_artificial_list");
-    test.setAttribute("id", test.value.concat("_selected"))
-    test.innerText = test.textContent;
-    test.appendChild(createRemoveTestButton(test.value));
+    var test_div = document.createElement("div");
+    test_div.setAttribute("class", "test_selection_artificial_list");
+    test_div.setAttribute("id", test.value.concat("_selected"));
+    test_div.innerText = test.textContent;
+    test_div.appendChild(createRemoveTestButton(test.value));
     return test;
 }
 
 function handleClickofAddTestButton() {
-    var current_selection = document.getElementById('test_client').value;
-    var options = document.getElementById('test_client').options;
+    var current_selection = document.getElementById("test_client").value;
+    var options = document.getElementById("test_client").options;
     var selected_test = Array.from(options).filter(e => e.value == current_selection)[0];
     removeNoneTextFromTestsSelected();
-    var tests_selected_artificial_list = document.getElementById('tests_selected_text_area');
-    tests_selected_text_area.append(addSelectedTestToList(selected_test));
+    var tests_selected_artificial_list = document.getElementById("tests_selected_text_area");
+    tests_selected_text_area.append(createDivForSelectedTest(selected_test));
 }
 
 function handleClickofRemoveTestButton(selection) {
-    var tests_selected_artificial_list = document.getElementById('tests_selected_text_area');
+    var tests_selected_artificial_list = document.getElementById("tests_selected_text_area");
     tests_selected_artificial_list.removeChild(selection);
     addNoneTextToTestsSelected();  
 }
@@ -54,7 +54,7 @@ document.addEventListener("click", function(e) {
         handleClickofAddTestButton();
     }
 
-    if(e.target.getAttribute('class') == "test_selection_artificial_list") {
+    if(e.target.getAttribute("class") == "test_selection_artificial_list") {
         console.log(e.parentNode);
         handleClickofRemoveTestButton(e.parentNode);
     }
