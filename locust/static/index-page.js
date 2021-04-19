c4bd532635ee0ce0d53d739a36ac297747bc2c13
Emily Edmondson <emily@bevy.com>
2021-04-19 14:22:20 -0500

    wip

--- locust/static/index-page.js
+++ locust/static/index-page.js
@@ -1,9 +1,9 @@
-document.addEventListener('click', function(e)) {
-	// if(e.target.getAttribute("id") == "specify_user_count") {
-	//   #selected_users = document.getElementsByTagName()
-	// }
-	console.log(e);
-	var test_name = document.getElementById("test_client");
-	console.log(test_name);
-	// <i class="far fa-times-circle"></i>
-}
+document.addEventListener("click", function(e) {
+    // if(e.target.getAttribute("id") == "specify_user_count") {
+    //   #selected_users = document.getElementsByTagName()
+    // }
+    console.log(e);
+    var test_name = document.getElementById("test_client");
+    console.log(test_name);
+    // <i class="far fa-times-circle"></i>
+}, false);
