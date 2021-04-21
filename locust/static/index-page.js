92ea51e23b2be118170c952e0685f3deaf305000
Emily Edmondson <emily@bevy.com>
2021-04-20 21:26:47 -0500

    wip

--- locust/static/index-page.js
+++ locust/static/index-page.js
@@ -177,7 +177,7 @@ function addTestToSpecifyGroup() {
     current_selection.appendChild(input);
 }
 
-ifIsInputElement(child, original_ele) {
+function ifIsInputElement(child, original_ele) {
     var name_is = child.attributes.name;
     var child_text = child.attributes.name.textContent.replace("_user_count", "");
     var original_text = original_ele.attributes.id.textContent.replace("_selected", "");
