4258fc298d7ea4c58c6efa21c0b71556f1939642
Emily Edmondson <emily@bevy.com>
2021-04-20 21:48:48 -0500

    wip

--- locust/static/index-page.js
+++ locust/static/index-page.js
@@ -161,9 +161,7 @@ function addTestToSpecifyGroup() {
     var selected_test = Array.from(options).filter(e => e.value == current_selection)[0];
     var input_container = document.getElementById("user-num-input-container");
     var user_count_text = selected_test.attributes.value.textContent.concat("_user_count");
-    console.log(input_container);
-    console.log(input_container.children);
-    if(input_container.children.length > 0 && input_container.children[0].textContent.includes("No tests selected :(")) {
+    if(input_container.children.length == 2 && input_container.children[0].textContent.includes("No tests selected :(")) {
         input_container.removeChild(input_container.chidlren[0]);
     }
     var label = generateLabelElement(
